630f3898674b14c73def678608b01ef320718af6
Min Cao <mcao4003@gmail.com>
2024-03-03 16:14:18 -0700

    add delete and update conact

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json());
 morgan.token('content', function (req, res) {
-    return JSON.stringify(req.body); // Return the request body as a JSON string
+  return JSON.stringify(req.body); // Return the request body as a JSON string
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 app.use(express.static('dist'))
-const cors = require('cors')
+const cors = require('cors');
 app.use(cors())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
-const generateId = () => {
-    const randomId = Math.floor(Math.random()*50)
-    return randomId
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if(error.name === "CastError"){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
 }
 
 // Get the current date
@@ -56,50 +38,67 @@ const timezone = currentDate.toString().match(/\((.*?)\)/)[1];
 // Construct the formatted string
 const formattedDate = `${day} ${month} ${year} ${time} GMT${currentDate.getTimezoneOffset() > 0 ? '+' : '-'}${Math.abs(currentDate.getTimezoneOffset()/60)}00 (${timezone})`;
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
-});
-
 app.get('/info', (req, res) => {
-    res.send(
-        `<br>Phonebook has info for ${persons.length} people<br> ${formattedDate}`
-    );
+  Person.find({}).then(persons => {
+    res.send(`<br>Phonebook has info for ${persons.length} people<br> ${formattedDate}`);
+  })
 });
 
-app.get('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person=>person.id===id)
-    if(person){
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+});
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person){
         res.json(person)
-    }else{
+      }else{
         res.status(404).end()
-    }
+      }
+    })
+    .catch(error=>next(error))
 })
 
-app.delete('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person=>person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(deletedPerson=>{
+      res.json(deletedPerson)
+    })
+    .catch(error=>next(error))
 })
 
 app.post('/api/persons', (req,res)=>{
-    const body = req.body
-    const isNameExisit = persons.find(person => person.name.includes(body.name))
-    if(!body.name || isNameExisit){
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
-    persons = persons.concat(person)
-    res.json(person)
+  const body = req.body
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(
+      savedPerson => {
+        res.json(savedPerson)
+      }
+    )
 })
 
-const PORT = 3001;
+app.put('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id;
+  const updatedPerson = req.body; // New information for the updated person
+    
+  Person.findByIdAndUpdate(id, updatedPerson, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson); // Send the updated person as JSON response
+    })
+    .catch(error => next(error));
+});
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT||3001;
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
